a32d02d3e0731f324c481dac3697f9dcea259d04
Brian Joe Kamau <joebrian998@gmail.com>
2024-10-01 10:41:00 +0300

    updating file

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const divide = function(a, b) {
     return a / b;
 };
 
-// Arrow function to calculate the square of a number
 const square = (x) => {
     return x * x;
 };
 
-// Arrow function to add two numbers
 const add = (a, b) => {
     return a + b;
 };
 
-// Exporting the functions to make them available for testing
 module.exports = { divide, square, add };
